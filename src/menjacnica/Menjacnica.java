3c50f75742d5e8c8bea35ee117b56196b8c354bf
jovanaveljovic <jovanaveljovic99@yahoo.com>
2015-03-25 23:04:20 +0100

    Dodata klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,23 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKursValute(String v, double pk, double kk, double sk) {
+	
+
+	}
+
+	public void obrisiKursValute(String v, GregorianCalendar d) {
+	
+	}
+
+	public Kurs pronadjiKursValute(String v, GregorianCalendar d) {
+		
+		return null;
+	}
+
+}
