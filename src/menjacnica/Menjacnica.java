bd9e4986d17fc7cb3c3cf5643c6be5d69f9e0b39
jovanaveljovic <jovanaveljovic99@yahoo.com>
2015-03-25 23:50:20 +0100

    Napravljena implementacija metode za dodavanje kursa valute

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,13 +1,26 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodajKursValute(String v, double pk, double kk, double sk, GregorianCalendar d) {
 	
+		for(int i = 0; i<valute.size(); i++){
+			if(valute.get(i).getNaziv().equals(v)){
+				Kurs k = new Kurs();
+				k.setDatum(d);
+				k.setKupovniKurs(kk);
+				k.setProdajniKurs(pk);
+				k.setSrednjiKurs(sk);
+				valute.get(i).getKursevi().add(k);
+			}
+		}
 
 	}
 
