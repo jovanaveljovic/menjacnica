20d8a59202b6b957d3003454263b8f9ac39a66a8
jovanaveljovic <jovanaveljovic99@yahoo.com>
2015-03-25 22:46:33 +0100

    Dodate metode equals, hashCode i toString u klase Valuta i Kurs

--- src/menjacnica/Kurs.java
+++ src/menjacnica/Kurs.java
@@ -4,7 +4,7 @@ import java.util.GregorianCalendar;
 
 public class Kurs {
 
-	GregorianCalendar datum;
+	private GregorianCalendar datum;
 	private double prodajniKurs;
 	private double kupovniKurs;
 	private double srednjiKurs;
@@ -33,6 +33,36 @@ public class Kurs {
 	public void setSrednjiKurs(double srednjiKurs) {
 		this.srednjiKurs = srednjiKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [datum=" + datum + ", prodajniKurs=" + prodajniKurs
+				+ ", kupovniKurs=" + kupovniKurs + ", srednjiKurs="
+				+ srednjiKurs + "]";
+	}
+	
 	
 	
 }
--- src/menjacnica/Valuta.java
+++ src/menjacnica/Valuta.java
@@ -27,6 +27,35 @@ public class Valuta {
 	public void setKursevi(LinkedList<Kurs> kursevi) {
 		this.kursevi = kursevi;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv
+				+ ", kursevi=" + kursevi + "]";
+	}
+	
 	
 	
 }
